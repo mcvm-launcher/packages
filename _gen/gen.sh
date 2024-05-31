b72d6e360e2e63ef1cc7465c9a43e20704698967
CarbonSmasher <crabonsmasher@gmail.com>
2024-05-31 17:51:02 -0400

    fix: Switch to using new plugin for generation

--- _gen/gen.sh
+++ _gen/gen.sh
@@ -1,4 +1,4 @@
 echo "Generating packages..."
 # Perform in parallel
 # parallel -a packages.txt --jobs 20 ./gen_one.sh
-mcvm_tools gen-pkg-batched config.json $@
+mcvm gen-pkg-batched config.json $@
